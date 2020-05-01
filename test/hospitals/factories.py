610ea87902683a3b5532fc5d5635005193406f24
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2020-05-01 13:48:46 -0300

    Fix tests

--- test/hospitals/factories.py
+++ test/hospitals/factories.py
@@ -1,13 +1,18 @@
 import factory
-from apps.hospitals.models import Hospital
+from apps.hospitals.models import Hospital, City
+
+
+class CityFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = City
 
 
 class HospitalFactory(factory.DjangoModelFactory):
     class Meta:
         model = Hospital
-    
+
+    city = factory.SubFactory(CityFactory)
     acronym = factory.Sequence(lambda n: f"Hospital #{n}")
     name = "Hospital of Konoha"
-    city = "Konoha"
     phonenumber = "1231232323"
     email = "konoha@hospital"
--- test/hospitals/test_view.py
+++ test/hospitals/test_view.py
@@ -4,19 +4,27 @@ from .factories import HospitalFactory
 
 pytestmark = pytest.mark.django_db
 
+
+def test_index(client):
+    url = reverse('hospital_list')
+    response = client.get(url)
+    assert response.status_code == 200
+
+
 def test_hospital_detail_status_code(client):
     hospital = HospitalFactory()
-    url = reverse('hospital-detail', args=[hospital.id])
-    response = client.get(url) 
+    url = reverse("hospital_view", args=[hospital.id])
+    response = client.get(url)
     assert response.status_code == 200
 
 
 def test_hospital_detail_templates(client, django_assert_num_queries):
     hospital = HospitalFactory()
-    url = reverse('hospital-detail', args=[hospital.id])
+    url = reverse("hospital_view", args=[hospital.id])
     with django_assert_num_queries(4):
         response = client.get(url)
     template_names = [template.name for template in response.templates]
-    assert "hospital/show.html" in template_names
+    assert "hospitals/hospital_detail.html" in template_names
     assert "base.html" in template_names
     assert "Hospital of Konoha" in str(response.content)
+
